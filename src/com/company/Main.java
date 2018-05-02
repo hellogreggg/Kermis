f1c553d5e0cbff1c6ae99d78dd1ddeb72a745182
hellogreggg <theawesomelifeofgreg@gmail.com>
2018-05-02 16:24:12 +0200

    second update before i leave class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String keuze = sc.nextLine();
-        System.out.println(keuze);
+
 
     }
 }
@@ -20,32 +18,58 @@ class  Kermis {
 class Attractie  extends  Kermis{
 
     public void Keuzen() {
+        Scanner sc = new Scanner(System.in);
+        String keuze = sc.nextLine();
+        System.out.println(keuze);
+        if (keuze == 1){
 
-        if (keuze == 1)
+        } else if (keuze == 2){
+
+        }
     }
 }
 
 class Reuzenrad extends Attractie {
 
+        void gekozen(){
+            System.out.println("You chose Reuzenrad ");
+        }
 }
 
 class Draaimolen extends Attractie {
+
+    void gekozen(){
+        System.out.println("You chose Reuzenrad ");
+    }
 
 }
 
 class Botsautos extends Attractie {
 
+    void gekozen(){
+        System.out.println("You chose Reuzenrad ");
+    }
+
 }
 
 class Spiegelpaleis extends Attractie {
+    void gekozen(){
+        System.out.println("You chose Reuzenrad ");
+    }
 
 }
 
 class Spookhuis extends  Attractie {
+    void gekozen(){
+        System.out.println("You chose Reuzenrad ");
+    }
 
 }
 
 class Klimmuur extends  Attractie {
+    void gekozen(){
+        System.out.println("You chose Reuzenrad ");
+    }
 
 }
 
